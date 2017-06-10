1f25f783a0132e97ddcfc0c2307bd781ad93d7c1
Vasileios Kougioumtzis <Vasileios Kougioumtzis>
2017-06-10 21:46:55 +0300

    initial commit for github

---
+++ src/actions/accountActions.js
@@ -0,0 +1,29 @@
+import * as types from './actionTypes';
+import mockApi from '../api/mockApi';
+import {beginAjaxCall, ajaxCallError} from './ajaxStatusActions';
+
+//Fires Login user success login action
+export function loginUserSuccess() {
+  return {type: types.LOGIN_USER_SUCCESS};
+}
+
+//Fires logout user action
+export function logout() {
+  return {type: types.LOGOUT_USER};
+}
+
+//Mock Api call , on successs triggers loginUserSuccess
+export function loginUser(user) {
+  return function (dispatch, getState) {
+    dispatch(beginAjaxCall());
+    return mockApi.loginUser(user).then(() => {
+     dispatch(loginUserSuccess()) 
+    })
+    .catch(error => {
+      dispatch(ajaxCallError(error));
+      throw(error);
+    });
+  };
+}
+
+//No api for Logout, will be client side
---
+++ src/reducers/accountReducer.js
@@ -0,0 +1,14 @@
+import * as types from '../actions/actionTypes';
+import initialState from './initialState';
+
+//Manages account actions
+export default function accountReducer(state = initialState.loggedIn, action){
+
+    if (action.type == types.LOGIN_USER_SUCCESS) {   
+    return 1;
+    } else if (action.type == types.LOGOUT_USER) {
+    return  0;
+    }
+
+  return state;
+}
